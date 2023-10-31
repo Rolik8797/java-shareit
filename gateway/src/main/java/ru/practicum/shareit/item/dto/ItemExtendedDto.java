0661d9c9c73dca763c99f52f3c3cd2a62bafd7b2
Светлана <kulechko@mail.ru>
2023-10-31 14:06:06 +0300

    ТЗ16

---
+++ gateway/src/main/java/ru/practicum/shareit/item/dto/ItemExtendedDto.java
@@ -0,0 +1,23 @@
+package ru.practicum.shareit.item.dto;
+
+import lombok.*;
+import ru.practicum.shareit.booking.dto.BookingItemDto;
+import ru.practicum.shareit.item.dto.CommentDto;
+
+import java.util.List;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class ItemExtendedDto {
+    Long id;
+    String name;
+    String description;
+    Boolean available;
+    Long ownerId;
+    Long requestId;
+    BookingItemDto lastBooking;
+    BookingItemDto nextBooking;
+    List<CommentDto> comments;
+}
