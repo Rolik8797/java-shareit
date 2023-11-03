c7bb487ab2123e8d9ce8f03e364ec3b4ae19815a
Светлана <kulechko@mail.ru>
2023-11-03 13:44:46 +0300

    ТЗ161

--- gateway/src/main/java/ru/practicum/shareit/constains/Constants.java
+++ gateway/src/main/java/ru/practicum/shareit/constains/Constants.java
@@ -5,5 +5,5 @@ public abstract class Constants {
     public static final String headerUserId = "X-Sharer-User-Id";
     public static final String pageFrom = "0";
     public static final String pageSize = "10";
-    public static final String headerServerUrl = "${shareit-server.url}";
+    public static final String headerServerUrl = "${SHAREIT_SERVER_URL}";
 }
